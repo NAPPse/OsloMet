ec42952d1a16d679b8da524b8093e5e836a54318
NAPPse <nappse@hotmail.com>
2018-03-12 00:52:52 +0100

    Create Lab7.ino

---
+++ Digitale Systemer/Lab/Lab7/Lab7.ino
@@ -0,0 +1,44 @@
+#include <Stepper.h>
+
+const int MAX_STEPS 360;
+Stepper stepper(MAX_STEPS, 8, 9, 10, 11);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Ready...");
+  stepper.setSpeed(30);
+}
+
+void loop() {
+  while(!Serial.available()){
+    delay(20);
+  }
+  for(int i=0; i<=1; i++) {
+    String dir = Serial.readStringUntil(' ');
+    int steps = Serial.readStringUntil('\n').toInt();
+    if(dir == "right" || dir == "speed" || dir == "left" && 0 < steps && steps < 256){
+      Serial.print("Dir/Speed:\t");
+      Serial.println(dir);
+      Serial.print("Steps/Value:\t");
+      Serial.println(steps);
+      stepper_cmd(dir[0], steps);
+      loop();
+    }
+    else{
+      Serial.println("Ugyldig kommando!");
+      loop();
+    }
+  }
+}
+
+void stepper_cmd(char dir, int steps){
+  switch(dir){
+    case 's':
+    Serial.println("Setter hastighet..");
+      stepper.setSpeed(steps);
+      break;
+    default:
+      Serial.println("Setter rettning..");
+      break;
+  }
+}
