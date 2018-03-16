3121b9bfcb1f95a437631d519166736953132609
NAPPse <nappse@hotmail.com>
2018-03-16 11:26:17 +0100

    Update Lab7.ino

--- Digitale Systemer/Lab/Lab7/Lab7.ino
+++ Digitale Systemer/Lab/Lab7/Lab7.ino
@@ -1,7 +1,7 @@
 #include <Stepper.h>
 
-const int MAX_STEPS 360;
-Stepper stepper(MAX_STEPS, 8, 9, 10, 11);
+const int STEPS_REV 648;
+Stepper stepper(STEPS_REV, 8, 9, 10, 11);
 
 void setup() {
   Serial.begin(9600);
@@ -39,6 +39,12 @@ void stepper_cmd(char dir, int steps){
       break;
     default:
       Serial.println("Setter rettning..");
+      switch(dir){
+        case 'l':
+          steps *= -1;
+          break;
+      } 
+      stepper.step(steps);
       break;
   }
 }
