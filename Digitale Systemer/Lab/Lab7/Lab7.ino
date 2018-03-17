972c70d5dbaf8cccf95ba855cb2fc886cec1a87f
NAPPse <nappse@hotmail.com>
2018-03-17 13:16:52 +0100

    Update Lab7.ino

--- Digitale Systemer/Lab/Lab7/Lab7.ino
+++ Digitale Systemer/Lab/Lab7/Lab7.ino
@@ -1,7 +1,7 @@
 #include <Stepper.h>
 #include <ctype.h>
 
-const int STEPS_REV 648; // Steps/rev
+#define STEPS_REV 648 // Steps/rev
 Stepper stepper(STEPS_REV, 8, 9, 10, 11);
 
 void setup() {
@@ -14,20 +14,18 @@ void loop() {
   while(!Serial.available()){ //Henger her til den får en seriell kommando
     delay(20);
   }
-  for(int i=0; i<=1; i++) {
-    String dir = Serial.readStringUntil(' ');
-    int steps = Serial.readStringUntil('\n').toInt();
-    if(dir == "right" || dir == "speed" || dir == "left" && isdigit(steps) && 0 < steps){
-      Serial.print("Dir/Speed:\t");
-      Serial.println(dir);
-      Serial.print("Steps/Value:\t");
-      Serial.println(steps);
-      stepper_cmd(dir[0], steps); //Sender første bokstav og N_steps/hastighetsverdi til stepper_cmd()
-      loop();
-    }
-    else{
-      Serial.println("Ugyldig kommando!");
-    }
+  String dir = Serial.readStringUntil(' ');
+  int steps = Serial.readStringUntil('\n').toInt();
+  if((dir == "right" || dir == "speed" || dir == "left") && 0 < steps){
+    Serial.print("Dir/Speed:\t");
+    Serial.println(dir);
+    Serial.print("Steps/Value:\t");
+    Serial.println(steps);
+    stepper_cmd(dir[0], steps); //Sender første bokstav og N_steps/hastighetsverdi til stepper_cmd()
+    loop();
+  }
+  else{
+    Serial.println("Ugyldig kommando!");
   }
 }
 
@@ -35,6 +33,10 @@ void stepper_cmd(const char dir, int steps){
   switch(dir){
     case 's': //Hvis første bokstav er s var kommandoen "speed"
       Serial.println("Setter hastighet..");
+      if(steps > 100){
+        Serial.println("Ro ned. Du kan ikke kjøre fortere enn 100%");
+        steps = 100;
+      }
       stepper.setSpeed(steps); //Setter ny hastighet
       return; //Returnerer fordi vi ikke ønsker at motoren skal gå n_steps for ny hastighetsverdi
     case 'l': //Hvis første bokstav er l var kommandoen "left"
@@ -46,4 +48,5 @@ void stepper_cmd(const char dir, int steps){
       break;
    }
    stepper.step(steps); //Kjører valgte steps.
+   Serial.println(steps);
 }
