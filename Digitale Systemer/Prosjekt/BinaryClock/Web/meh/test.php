d9f3a306c590917322e07ff4692521fda572d44b
NAPPse <nappse@hotmail.com>
2018-10-05 22:37:31 +0200

    Update test.php

--- Digitale Systemer/Prosjekt/BinaryClock/Web/meh/test.php
+++ Digitale Systemer/Prosjekt/BinaryClock/Web/meh/test.php
@@ -1,13 +1,15 @@
 <?php
-function Redirect($url, $permanent = false)
-{
-    if (headers_sent() === false)
-    {
-        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
-    }
+$url = 'https://www.nordicmafia.org/index.php?p=game_dice';
+$data = array('innsats' => '100', 'password' => 'hemmelig', 'passwordProtected' => '1', 'players' => '2', 'startGame' => 'Start+spill');
+$options = array(
+        'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    )
+);
 
-    exit();
-}
-
-Redirect('http://www.google.com/', false);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+var_dump($result);
 ?>
